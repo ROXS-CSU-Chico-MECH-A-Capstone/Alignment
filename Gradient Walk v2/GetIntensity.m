fb63eee073079d302ab4cde2c704d77a94473bd2
WestJeremy <116618595+WestJeremy@users.noreply.github.com>
2023-03-08 10:33:03 -0800

    added matlabsim file

--- Gradient Walk v2/GetIntensity.m
+++
@@ -1,4 +0,0 @@
-function z=GetIntensity(x,y,x0,y0) %get intensity on coords x and y with resective offsets xo and yo
-R = sqrt((x-x0).^2 + (y-y0).^2) + eps;
-z = sin(R)./R;
-end
--- Gradient Walk v8/Build_Spiral.m
+++
@@ -1,9 +0,0 @@
-function [S,revs,k]=Build_Spiral(spacing,Max_Radius,points,x0,y0)
-revs=Max_Radius/spacing;                    %calc number of revolutions
-k=spacing/(2*pi);                       
-theta=linspace(0,revs*2*pi,points); %how many points and angle coord
-r=k*theta;                       %radius coord
-x=r.*cos(theta)+x0;                 %convert to x cartesian
-y=r.*sin(theta)+y0;                 %convert to y cartesian
-S=[x',y'];
-end
